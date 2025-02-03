af666ab8b6a36157ca93c931aec74fe7dbcffbc9
Ujjwal Shah <ujjwal.2201150ec@iiitbh.ac.in>
2025-02-03 17:39:05 +0530

    updated routes

---
+++ backend/routes/account.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const {authMiddleware} = require("../middleware");
+const {Account} = require("../db");
+const mongoose = require("mongoose");
+
+router.get("/balance" , authMiddleware , async (req,res)=> {
+    const account = await Account.findOne({
+        userId: req.userId
+    });
+
+    res.json({
+        balance: account.balance,
+    })
+});
+
+router.post("/transfer" , authMiddleware , async (req,res)=> {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    const {amount , to} = req.body;
+    const from = req.userId;
+
+    const account = await Account.findOne({
+        userId: from
+    }).session(session);
+
+    if(!account || account.balance<amount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Insufficient balance"
+        });
+    }
+
+    const toAccount = await Account.findOne({
+        userId: to,
+    }).session(session);
+
+    if(!toAccount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Invalid account"
+        });
+    }
+
+    await Account.updateOne({
+        userId : from,
+    } , {
+        $inc : {balance : -amount}
+    }).session(session);
+
+    await Account.updateOne({
+        userId : to,
+    } , {
+        $inc: {balance : amount}
+    }).session(session);
+
+    await session.commitTransaction();
+
+    res.json({
+        message: "Transfer successful"
+    });
+
+})
+
+module.exports = {
+    router
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const zod = require("zod");
 const router = express.Router();
-const {User} = require("../db");
+const {User , Account} = require("../db");
 const jwt = require("jsonwebtoken");
 const {JWT_SECRET} = require("../config");
 const {authMiddleware} = require("../middleware");
@@ -32,6 +32,11 @@ router.post("/signup" , async (req,res) => {
     }
 
     const dbUser = await User.create(body);
+
+    await Account.create({
+        userId: dbUser._id,
+        balance: 1 + Math.random()*10000,
+    })
 
     const token = jwt.sign({
         userId : dbUser._id
